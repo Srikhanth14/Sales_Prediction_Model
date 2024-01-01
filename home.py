cef2f30908c0786db48425ceb15790f2d5de36e3
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2024-01-01 12:05:22 +0530

    Update home.py

--- home.py
+++ home.py
@@ -34,7 +34,7 @@ def home():
     # About Me
     st.subheader("About Me:")
     st.write('''Curious about the mind behind the model? I'm SRIKHANTH R, a passionate data
-    scientist dedicated to leveraging technology for insightful predictions.''')
+    analyst dedicated to leveraging technology for insightful predictions.''')
     st.write("Feel free to reach out if you have questions, collaborations, or just want to chat about data science!")
     st.write("Check out my [portfolio](https://www.datascienceportfol.io/srikhanth_r) to explore more of my data science projects.")
     st.write("Connect with me on [LinkedIn](https://www.linkedin.com/in/srikhanth-r). I'd love to hear from you!")
