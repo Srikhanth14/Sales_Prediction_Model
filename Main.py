b74a62ccc3ea2c631409d71a2e6341fd4e454ecd
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2024-01-01 18:48:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ Created on Sun Dec 31 14:51:40 2023
 
 from streamlit_option_menu import option_menu
 import home, Visualization, Dataset, Input_Form
+st.set_page_config(page_title="Sales Prediction", page_icon="coin", layout="wide")
 
 selected = option_menu(
                         menu_title="Sales",
